b5e17f9e50f3720b36af15b5f0267ea3c0bc9eb1
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-19 19:48:28 -0300

    :sparkles: feat: App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import "./global.scss";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
